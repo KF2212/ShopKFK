b9048af01cc90925f0d28ae5b7aeb77915b756f1
Krzysztof Czaja <krzysztof.czaja-97@wp.pl>
2019-03-28 16:55:13 +0100

    Jwt Api

---
+++ OnlineShop/JwtAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("token")]
+        public ActionResult GetToken()
+        {
+            string securityKey = "123414125124123123124123124123123123.in";
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+            var token = new JwtSecurityToken(
+                    issuer: "smeck.in",
+                    audience: "readers",
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: signingCredentials,
+                    claims: claims
+                );
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
