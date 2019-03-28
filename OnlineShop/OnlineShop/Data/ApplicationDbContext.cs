aca3f96b389ffea458e72bb53a3a84874f9a9126
filipxd4 <filipwerochowski@icloud.com>
2019-03-28 18:40:30 +0100

    Login in progress

--- OnlineShop/OnlineShop/Data/ApplicationDbContext.cs
+++ OnlineShop/OnlineShop/Data/ApplicationDbContext.cs
@@ -9,19 +9,19 @@ using System.Threading.Tasks;
 
 namespace OnlineShop.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        //    : base(options)
+        //{
+        //}
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Core Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Core Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-        }
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //    // Customize the ASP.NET Core Identity model and override the defaults if needed.
+        //    // For example, you can rename the ASP.NET Core Identity table names and more.
+        //    // Add your customizations after calling base.OnModelCreating(builder);
+        //}
     }
 }
---
+++ OnlineShop/OnlineShop/Models/DetailContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop
+{
+    public class DetailContext:DbContext
+    {
+        public DetailContext(DbContextOptions<DetailContext> options) : base(options)
+        {
+
+        }
+        public DetailContext()
+        {
+
+        }
+        //public DbSet<Contact> Contacts { get;set; }
+        public DbSet<SignUp> signUp { get;set; }
+    }
+}
