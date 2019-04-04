519a90d3d535354a2c5a149ee06197baee48d0d1
Krzysztof Czaja <krzysztof.czaja-97@wp.pl>
2019-04-04 18:43:16 +0200

    Seeded Data

---
+++ OnlineShop/OnlineShop/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineShop.Models;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new OnlineShopContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<OnlineShopContext>>()))
+            {
+                
+                //if (context.Products.Any())
+                //{
+                //    return;   
+                //}
+
+                context.Products.AddRange(
+                    new Products
+                    {
+                        Name = "Kurtka",
+                        Description = "Zimowa",
+                        Price = 120
+                    },
+                    new Products
+                    {
+                        Name = "Kurtka",
+                        Description = "Wiosenna",
+                        Price = 120
+                    },
+                    new Products
+                    {
+                        Name = "Kurtka",
+                        Description = "Jesienna",
+                        Price = 120
+                    },
+                    new Products
+                    {
+                        Name = "Kurtka",
+                        Description = "Letnia",
+                        Price = 120
+                    },
+                    new Products
+                    {
+                        Name = "Spodnie",
+                        Description = "Zimowe",
+                        Price = 120
+                    },
+                    new Products
+                    {
+                        Name = "Spodnie",
+                        Description = "Letnie",
+                        Price = 120
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- OnlineShop/OnlineShop/Program.cs
+++ OnlineShop/OnlineShop/Program.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+using OnlineShop.Models;
 
 namespace OnlineShop
 {
@@ -14,7 +18,25 @@ namespace OnlineShop
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<OnlineShopContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
